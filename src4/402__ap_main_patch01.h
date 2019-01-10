b5ef81d9b3ca7ce6b8bc3b8519f93f3d85c12d1c
Your Name <you@example.com>
2019-01-10 23:13:26 +0800

    _2019_0110__2313pm

--- src4/402__ap_main_patch01.h
+++ src4/402__ap_main_patch01.h
@@ -1,3 +1,21 @@
+/*
+ *  *********************************************
+ *  Usage : replace the main() as following 
+ *  *********************************************
+ *
+// defined "_useMyMain01" inside the following h file
+#include    "ap_main_patch01.h"
+
+int16 main(int16 param)
+{
+#ifdef _useMyMain01
+    _mPatch01();
+}
+int16 main_fake(int16 param)
+{
+#endif
+*
+*/
 
 #ifndef     _useMyMain01
 #define     _useMyMain01
