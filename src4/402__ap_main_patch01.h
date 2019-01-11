04605b9e3898cc672592643dca32afb71e219694
Your Name <you@example.com>
2019-01-11 16:56:06 +0800

    _2019_0111__1656pm

--- src4/402__ap_main_patch01.h
+++ src4/402__ap_main_patch01.h
@@ -26,17 +26,22 @@ int16 main_fake(int16 param)
 #include "test01_uart_and_delay.h"
 #include "test02_uart_as_start_detect.h"
 
+int16 main_fake(int16 param);
 void _mPatch01(void) {
 
     SetPLL(PLL_48MHZ);
     SetVDD(VDDSET_1_6V);
     ClearWatchDog();
 
-    while ( 1 ) {
+    while ( 0 ) {
         ClearWatchDog();
         //if ( _test01_uart_and_delay() ) return ;
         if ( _test02_uart_as_start_detect() ) return ;
     }
+
+    // call the real main loop
+    main_fake(param);
+
 } // _mPatch01
 
 #endif
