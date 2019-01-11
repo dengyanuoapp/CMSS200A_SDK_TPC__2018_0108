43b706c8f061d057ce04a999ef682491d6333ff8
Your Name <you@example.com>
2019-01-11 19:34:16 +0800

    _2019_0111__1934pm

--- src4/404__test02_uart_as_start_detect.h
+++ src4/404__test02_uart_as_start_detect.h
@@ -2,6 +2,13 @@
 #ifndef     __test02_uart_as_start_detect_h
 #define     __test02_uart_as_start_detect_h
 
+_reset01( void ) {
+    IE0 &= 0x7f;
+    SFR_BANK = BANK_RTC;
+    WDCTL =  0x8a; //watchdog使能并重启
+    while (1) { }
+} // _reset01
+
 // test_uart
 uint8 _test02_uart_as_start_detect(void)
 {
@@ -15,6 +22,10 @@ uint8 _test02_uart_as_start_detect(void)
     SFR_BANK = BANK_GPIO;
     GPIOCINEN  |= _maskRX ;
 
+    if (GPIOCDAT & _maskRX ) {
+        return 1 ; // skip , to run normal main.
+    }
+
     __tUartCnt01 = 0 ;
     while ( 1 ) {
         if (GPIOCDAT & _maskRX )
@@ -22,6 +33,7 @@ uint8 _test02_uart_as_start_detect(void)
             // 1000 : 1.63 ms , 61349 : 100 ms
             if ( __tUartCnt01 > 61349 ) { 
                 //__tUartCnt01 = 0 ;
+                _reset01();
                 break ;
             } else {
                 __tUartCnt01 ++ ;
